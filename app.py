7dd1a5dd052c03056c0a41f0e1c96fa444dbd3bf
Matt Davies <matt.davies1@mac.com>
2021-02-24 17:22:57 -0500

    update app.py

--- app.py
+++ app.py
@@ -1,34 +1,49 @@
 # Dependancies
 import matplotlib.pyplot as plt
 from matplotlib import image
-
 from skimage.filters import threshold_otsu
 from skimage.measure import label, regionprops
 from skimage.morphology import closing, square
 from skimage.color import label2rgb
 from skimage.color import rgb2gray
+import os
+import glob
 
-image = image.imread("IMG_1452.jpg")
-cropped_img = image[0:-100, 350:-100]
+
+
+
+# Smarter Image read-in functioinality
+path = os.path.join('inputs', '*.jpg')
+
+filepath = glob.glob(path)
+filepath
+
+image = image.imread(filepath[0])
+start_image = image
+print("Image loaded")
 
 # apply threshold
-image = rgb2gray(cropped_img) 
+image = rgb2gray(image)
 thresh = threshold_otsu(image)
 bw = closing(image > thresh, square(3))
 
 # add image labels to identify slides
 label_image = label(bw)
 
-# iterate through objects to output desired selections
+counter = 0
+
 for region in regionprops(label_image):
     # take regions with large enough areas
     if region.area >= 75000:
         # draw rectangle around segmented coins
         minr, minc, maxr, maxc = region.bbox
-        # create output figure
         fig, ax = plt.subplots(figsize=(12, 4))
-        plt.imshow(cropped_img[minr:maxr,minc:maxc])
+        plt.imshow(start_image[minr:maxr,minc:maxc])
         ax.set_axis_off()
         plt.tight_layout()
-        # save to outputs
-        fig.savefig(f'outputs/slide{regionprops(label_image).index(region)}.png')
+        # plt.show()
+        fig.savefig(f'outputs/output{counter + 1}.png')
+        print(f'Parsing slide {counter + 1} ...')
+        counter += 1
+
+print(f'Parser identified {counter} slides')
