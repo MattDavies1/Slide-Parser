40858b68b8809ab2bcc59b4efa9de3b83813351b
Sina Sartip <ssartipz@ryerson.ca>
2022-06-27 20:35:00 -0400

    brought back button commands

--- view/gui.py
+++ view/gui.py
@@ -38,7 +38,7 @@ class GUI():
         feet_entry.grid(column=2, row=3, sticky=(W, E))
         
         ttk.Label(self.mainframe, text="Input").grid(column=1, row=3, sticky=(E,W))
-        self.choose_input_loc_button = ttk.Button(self.mainframe, text="...", command=chooseInputFile)
+        self.choose_input_loc_button = ttk.Button(self.mainframe, text="...", command=self.choose_input_file)
         self.choose_input_loc_button.grid(column=3, row=3, sticky=(E,W))
 
         #Output Location text entry widget
@@ -47,25 +47,25 @@ class GUI():
         feet_entry.grid(column=5, row=3, sticky=(W, E))
         
         ttk.Label(self.mainframe, text="Output").grid(column=4, row=3, sticky=(E,W))
-        self.choose_output_loc_button = ttk.Button(self.mainframe, text="...", command=chooseOutputFolder)
+        self.choose_output_loc_button = ttk.Button(self.mainframe, text="...", command=self.choose_output_folder)
         self.choose_output_loc_button.grid(column=6, row=3, sticky=(E,W))
 
         # parsing button widget
-        self.preview_button = ttk.Button(self.mainframe, text="preview", command=startParsingImage)
+        self.preview_button = ttk.Button(self.mainframe, text="preview", command=self.start_parsing_image)
         self.preview_button.grid(columnspan=3, column=1, row=4, sticky=(E,W))
 
         # preview button widget
-        self.parse_button = ttk.Button(self.mainframe, text="parse", command=showPreviewParser)
+        self.parse_button = ttk.Button(self.mainframe, text="parse", command=self.show_preview_image)
         self.parse_button.grid(columnspan=4, column=4,row=4, sticky=(E,W))
 
         for child in self.mainframe.winfo_children(): 
             child.grid_configure(padx=5, pady=5)
         
-def chooseInputFile():
-    print("choose input file")
-def chooseOutputFolder():
-    print("choose output file")
-def showPreviewParser():
-    print("choose preview image")
-def startParsingImage():
-    print("choose parsing start")
+    def choose_input_file(self):
+        print("choose input file")
+    def choose_output_folder(self):
+        print("choose output file")
+    def show_preview_image(self):
+        print("choose preview image")
+    def start_parsing_image(self):
+        print("choose parsing start")
