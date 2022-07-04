d2aeaaca7cba4e081e843f99a6cd2b6b5ff460d9
Sina Sartip <ssartipz@ryerson.ca>
2022-07-04 12:01:30 -0400

    corrected is_same_path

--- controller/control.py
+++ controller/control.py
@@ -29,7 +29,7 @@ def is_same_path(source_file_path, target_dir_path):
     result = None
     if source_file_path.rfind(target_dir_path) != -1:
         if source_file_path.rfind(target_dir_path) == 0:
-            test_obj_1 = file_path.split(target_dir_path)[1]
+            test_obj_1 = source_file_path.split(target_dir_path)[1]
             test_obj_2 = test_obj_1.split('/')[1]
             test_obj_3 = test_obj_2.rfind('/')
             if test_obj_3 == -1:
